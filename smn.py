f35261ce935b83189034c40082fe74ecd18157f2
De <spark49942@gmail.com>
2022-10-20 20:49:54 +0300

    DZ-2

--- smn.py
+++ smn.py
@@ -1,38 +1,43 @@
+print('Здравствуйте!', end=' ')
 with open('homework.txt', 'r') as hw:
     hwtxt = hw.readlines()
 with open('lessons.txt', 'r') as lsn:
     lsntxt = lsn.readlines()
 while True:
-    menu = input('\nЗдравствуйте! Выберите желаемый пункт меню:\n(a) Студент\n(b) Преподаватель\n(c) Администратор\n(e) Выход\n\n')
+    if i == 1:
+        print('\nГотово!', end=' ')
+    menu = input('Выберите желаемый пункт меню:\n(a) Студент\n(b) Преподаватель\n(c) Администратор\n(e) Выход\n\n')
     if menu == 'a':
         i = 0
-        login = input('Пожалуйста, введите свои имя пользователя и пароль!\n\nИмя пользователя: ')
+        login = input('\nПожалуйста, введите свои имя пользователя и пароль!\n\nИмя пользователя: ')
         studword = input('Пароль: ')
         while i == 0:
-            answ = input(login, '- пожалуйста, выберите желаемое действие:\n1. Посмотреть расписание\n2. Посмотреть домашнее задание\n\n')
+            print(login, end=' - ')
+            answ = input('пожалуйста, выберите желаемое действие:\n\n(1) Посмотреть расписание\n(2) Посмотреть домашнее задание\n\n')
             if answ == '1':
-                print(lsntxt)
+                print(*lsntxt)
                 i += 1
             elif answ == '2':
-                print(hwtxt)
+                print(*hwtxt)
                 i += 1
             else:
                 print('Ошибка!')
     elif menu == 'b':
         i = 0
-        login = input('Пожалуйста, введите свои имя пользователя и пароль!\n\nИмя пользователя: ')
+        login = input('\nПожалуйста, введите свои имя пользователя и пароль!\n\nИмя пользователя: ')
         teachword = input('Пароль: ')
         while i == 0:
-            answ = input(login, '- пожалуйста, выберите желаемое действие:\n1. Посмотреть расписание\n2. Посмотреть домашнее задание\n3. Добавить домашнее задание\n\n')
+            print(login, end=' - ')
+            answ = input('пожалуйста, выберите желаемое действие:\n\n(1) Посмотреть расписание\n(2) Посмотреть домашнее задание\n(3) Добавить домашнее задание\n\n')
             if answ == '1':
-                print(lsntxt)
+                print(*lsntxt)
                 i += 1
             elif answ == '2':
-                print(hwtxt)
+                print(*hwtxt)
                 i += 1
             elif answ == '3':
                 put = 1
-                print('Напишите дату сдачи, предмет и домашнее задание:')
+                print('\nНапишите дату сдачи, предмет и домашнее задание:')
                 with open('homework.txt', 'a') as work:
                     while put > 0:
                         if put > 1:
@@ -50,7 +55,7 @@ while True:
             else:
                 print('Ошибка!')
     elif menu == 'e':
-        print('До свидания!')
+        print('\nДо свидания!')
         break
     else:
-        print('Ошибка!')
+        print('\nОшибка!')
